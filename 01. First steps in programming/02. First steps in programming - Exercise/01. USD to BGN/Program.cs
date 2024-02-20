4b385a3969d587d25447fa681cde3eb2ce68d72c
Valentin Karakashev <116033694+ValchoPalcho@users.noreply.github.com>
2024-02-20 14:09:51 +0200

    Changed!

---
+++ 01. First steps in programming/02. First steps in programming - Exercise/01. USD to BGN/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._USD_to_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
